73e543ec8c9e260b83eab0f7b30b51e04da53aab
ms elouafi <malfunction.pl-mn@outlook.com>
2014-11-03 23:10:24 +0100

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300*300>", :thumb => "100*100>" }
+
+	 validates :description, presence: true
 end
